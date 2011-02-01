ba6abd08cad0fdb541b49a18677522e4e5cae50c
Rolf Wuerdemann <Rolf.Wuerdemann@fmf.uni-freiburg.de>
2011-02-01 15:13:40 +0000

    fmftable.py transferred to nose

--- test/test_fmftable.py
+++ test/test_fmftable.py
@@ -42,6 +42,62 @@ class TestTableDefinitions():
         eq_(data_list[3], "; some stupid comment")
         eq_(data_list[4], "Beam Current: I_B(U_1) [mA]")
 
+    def test_data_definition_set(self):
+        a=[]
+        a.append(FMFDataDefinition(name="Bias Voltage", definition="U_1 [mV]"))
+        a.append(FMFDataDefinition(name="Beam Current", 
+            definition="I_B(U_1) [mA]"))
+        self.table.data_definitions = a
+        data_list = self.table.data_definitions
+        assert_true(isinstance(data_list[0], FMFDataDefinition))
+        assert_true(isinstance(data_list[1], FMFDataDefinition))
+        data_list = self.table.table_definition(";")
+        eq_(data_list[0], "[*data definitions: \tau]")
+        eq_(data_list[1], "Bias Voltage: U_1 [mV]")
+        eq_(data_list[2], "Beam Current: I_B(U_1) [mA]")
+
+    def test_data_table_mask_auto(self):
+        a=[]
+        a.append(FMFDataDefinition(name="Measuring Point", 
+            definition="n"))
+        a.append(FMFDataDefinition(name="Bias Voltage", 
+            definition="U_1(n) [mV]"))
+        a.append(FMFDataDefinition(name="Shutter", 
+            definition="s(n)"))
+        a.append(FMFDataDefinition(name="Direction", 
+            definition="d(n)"))
+        self.table.data_definitions = a
+        self.table.data=[
+                (0,1),
+                (0.1, 1.4568e-10),
+                (True, False),
+                ("North", "South")]
+        data_list = self.table.table_data("\t")
+        eq_(data_list[0], "[*data: \tau]")
+        eq_(data_list[1], '0\t1.000e-01\tTrue\tNorth')
+        eq_(data_list[2], '1\t1.457e-10\tFalse\tSouth')
+
+    def test_data_table_mask_supplied(self):
+        a=[]
+        a.append(FMFDataDefinition(name="Measuring Point", 
+            definition="n", mask="%02d"))
+        a.append(FMFDataDefinition(name="Bias Voltage", 
+            definition="U_1(n) [mV]", mask="%.1e"))
+        a.append(FMFDataDefinition(name="Accelerator Voltage", 
+            definition="U_2(n) [V]", mask="%.2f"))
+        a.append(FMFDataDefinition(name="Direction", 
+            definition="d(n)", mask="\"%s\""))
+        self.table.data_definitions = a
+        self.table.data=[
+                (0,1),
+                (0.1, 1.4568e-10),
+                (0.1, 0.001),
+                ("North", "South")]
+        data_list = self.table.table_data("\t")
+        eq_(data_list[0], "[*data: \tau]")
+        eq_(data_list[1], '00\t1.0e-01\t0.10\t\"North\"')
+        eq_(data_list[2], '01\t1.5e-10\t0.00\t\"South\"')
+
 class TestTableConsistency():
     def setUp(self):
         self.table=FMFTable(name="Test", symbol="\tau")
@@ -60,9 +116,3 @@ class TestTableConsistency():
     def test_consistency_ok(self):
         self.table.data=[(1,2,3),(1,2,3)]
         assert_true(self.table.verify_consistency())
-
-# class TestTableMask():
-#    def setUp(self):
-        self.table=FMFTable(name="Test", symbol="\tau")
-        self.table.add_data_definition({'Bias Voltage':'U_1 [mV]'})
-        self.table.add_data_definition('Acceleration Voltage','U_2(U_1) [kV]')
