b9071dc8e66d3d901f2e1beaf5e603fac1190820
Rolf Würdemann <Rolf.Wuerdemann@fmf.uni-freiburg.de>
2011-05-27 16:45:52 +0200

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8; -*-
+# from distutils.core import setup
+from setuptools import setup, find_packages
+setup(name='simplefmf',
+    description="An easy way to write fmf files",
+    long_description="""The Full Meta-Data Format is specified in
+    
+    http://arxiv.org/abs/0904.1299 
+    
+    This module gives a hackish way to write it.""",
+    version='0.1',
+    author="Rolf Würdemann",
+    author_email="Rolf.Wuerdemann@fmf.uni-freiburg.de",
+    maintainer="Rolf Würdemann",
+    maintainer_email="Rolf.Wuerdemann@fmf.uni-freiburg.de",
+    license = "BSD",
+    url="http://arxiv.org/abs/0904.1299",
+    py_modules=['simplefmf'],
+    packages = find_packages(),
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Topic :: Utilities",
+        "Environment :: Console",
+        "License :: OSI Approved :: BSD License",
+     ],
+    zip_safe=True,
+
+)
+
+
